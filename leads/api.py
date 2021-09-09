aa2c7c451e0341d34d0b4e5366e415a0b6a0c649
Choiinsung <redfox276@naver.com>
2021-09-09 13:32:45 +0900

    인증 추가

--- leads/api.py
+++ leads/api.py
@@ -3,8 +3,14 @@ from rest_framework import viewsets, permissions
 from .serializers import  LeadSerializer
 
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+    # lead를 create할때 우리가 lead owner를 저장하는것을 허락한다.
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
