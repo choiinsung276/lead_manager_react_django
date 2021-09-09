5c6f7d1be664d54a97b76fa81af38a0b7e79652b
Choiinsung <redfox276@naver.com>
2021-09-09 17:06:29 +0900

    acount앱 추가, 등록api 구현

--- accounts/api.py
+++ accounts/api.py
@@ -10,11 +10,11 @@ class RegisterAPI(generics.GenericAPIView):
     # request에 정보를 받아서
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_vail(raise_exception = True)
+        serializer.is_valid(raise_exception = True)
         user = serializer.save()
         return  Response({
             "user":UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)
+            "token": AuthToken.objects.create(user)[1]
         })
 
 
