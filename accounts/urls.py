d5be6bd1e70808b0237fd5116f7edb7b0bfaceeb
Choiinsung <redfox276@naver.com>
2021-09-09 18:31:10 +0900

    logout 추가 및 5강 끝

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI
+from .api import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 
 urlpatterns =[
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()), # 제네릭 뷰라서 as_view
-    path('api/auth/login', LoginAPI.as_view())
+    path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/user', UserAPI.as_view()),
+    path('api/auth/logout',knox_views.LogoutView.as_view(), name='knox_logout')#knox 가 로그아웃 구현해놓음
 ]
